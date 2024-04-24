6082eff9d8be2749cf689dbce5ae99af9a0930be
Duy Tran <duy.tranquoc@gforces.auto>
2024-04-24 10:47:08 +0700

    [symfony] basic CRUD

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ProductController extends AbstractController
+{
+    #[Route('/products', name: 'product_list')]
+    public function index(
+        ProductRepository $productRepository
+    ): Response
+    {
+        return $this->render('product/index.html.twig', [
+            'products' => $productRepository->findAll()
+        ]);
+    }
+
+    #[Route('/product/add', name: 'product_new')]
+    public function createProduct(
+        EntityManagerInterface $entityManager
+    ): Response {
+        $product = new Product();
+        $product->setName('Keyboard-updated');
+        $product->setPrice(1999);
+        $product->setDescription('Ergonomic and stylish! -updated');
+
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($product);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return new Response('Saved new product with id '.$product->getId());
+    }
+// show() or showDetail() can be used to show a single product BUT with different ways to get the product for reference
+
+    #[Route('/product/{id}', name: 'product_show')]
+    public function show(EntityManagerInterface $entityManager, int $id): Response
+    {
+        $product = $entityManager->getRepository(Product::class)->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+
+        return new Response('Check out this great product: '.$product->getName());
+
+        // or render a template
+        // in the template, print things with {{ product.name }}
+        // return $this->render('product/show.html.twig', ['product' => $product]);
+    }
+
+    #[Route('/product/show/{id}', name: 'product_show')]
+    public function showDetail(ProductRepository $productRepository, int $id): Response
+    {
+        $product = $productRepository->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+
+        return new Response('Check out this great product: '.$product->getName());
+
+        // or render a template
+        // in the template, print things with {{ product.name }}
+        // return $this->render('product/show.html.twig', ['product' => $product]);
+    }
+
+    #[Route('/product/edit/{id}', name: 'product_edit')]
+    public function update(EntityManagerInterface $entityManager, int $id): Response
+    {
+        $product = $entityManager->getRepository(Product::class)->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+
+        $product->setName('New product name!');
+        $entityManager->flush();
+
+        return $this->redirectToRoute('product_show', [
+            'id' => $product->getId()
+        ]);
+    }
+
+    #[Route('/product/delete/{id}', name: 'product_delete')]
+    public function delete(EntityManagerInterface $entityManager, int $id): Response
+    {
+        $product = $entityManager->getRepository(Product::class)->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+        $entityManager->remove($product);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('product_list');
+    }
+}
