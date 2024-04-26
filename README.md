724766916e630e4064dbb2df0dd4fa82874e1568
Duy Tran <duy.tranquoc@gforces.auto>
2024-04-26 18:17:34 +0700

    [symfony] 3 ways to inject service

--- README.md
+++ README.md
@@ -34,4 +34,7 @@ sudo apt install php-mysql
 symfony console doctrine:migrations:migrate
 ````
 
-### Technically speaking, Symfony Flex is a Composer plugin that is installed by default when creating a new Symfony application and which automates the most common tasks of Symfony applications.
+### Technically speaking, Symfony Flex is a Composer plugin that is installed by default when creating a new Symfony application and which automates the most common tasks of Symfony applications.\
+#### If you extend the base AbstractController, you can only get access to the most common services (e.g twig, router, doctrine, etc.), directly from the container via $this->container->get(). these services can be found under AbstractController->getSubscribedServices().
+unless we have to inject the service manually by:  type-hinting action method arguments (pass the serice directly to the function) OR via construct()
+ 
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,27 +4,40 @@ namespace App\Controller;
 
 use App\Kernel;
 use App\Services\MessageGenerator;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/home')]
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    public function __construct(
+        private LoggerInterface $logger,
+    ) {
+    }
+    #[Route('/', name: 'app_home')]
     public function index(
         Request $request,
-        MessageGenerator $messageGenerator,
-        Kernel $kernel
+        MessageGenerator $messageGenerator
     ): Response
     {
-//        dump($kernel->getLogDir());
-//        dump($kernel->getCacheDir());
-//        dump($kernel->getProjectDir());
-//        dd($kernel->getEnvironment());
 //        throw new \Exception('Something went wrong!');
+        $this->logger->info('home/index route is called'); // second way: inject via constructor
          return $this->render('home/index.html.twig', [
              'message' => $messageGenerator->getHappyMessage(),
+             'app_home_test_kernel_url' =>
+                 $this->container->get('router')->generate('app_home_test_kernel') // first way: by function from AbstractController
          ]);
+    }
+    #[Route('/test-kernel', name: 'app_home_test_kernel')]
+    public function testKernel(
+        Kernel $kernel // third way: inject via function
+    ) {
+        dump($kernel->getLogDir());
+        dump($kernel->getCacheDir());
+        dump($kernel->getProjectDir());
+        dd($kernel->getEnvironment());
     }
 }
